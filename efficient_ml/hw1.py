e7e6d49d7499a63b2acd30b2e4fef6f4979415c1
edward <awe31402@gmail.com>
2026-05-26 23:06:55 +0800

    hw1 - question 6
    Signed-off-by: edward <awe31402@gmail.com>

--- efficient_ml/hw1.py
+++ efficient_ml/hw1.py
@@ -284,6 +284,60 @@ class FineGrainedPruner:
                 masks[name] = fine_grained_pruning(param, sparsity_dict[name])
         return masks
 
+def get_num_channels_to_keep(channels: int, prune_ratio: float) -> int:
+    """A function to calculate the number of layers to PRESERVE after pruning
+    Note that preserve_rate = 1. - prune_ratio
+    """
+    ##################### YOUR CODE STARTS HERE #####################
+    return int(round(channels * (1. - prune_ratio)))
+    ##################### YOUR CODE ENDS HERE #####################
+
+@torch.no_grad()
+def channel_prune(model: nn.Module,
+                  prune_ratio: Union[List, float]) -> nn.Module:
+    """Apply channel pruning to each of the conv layer in the backbone
+    Note that for prune_ratio, we can either provide a floating-point number,
+    indicating that we use a uniform pruning rate for all layers, or a list of
+    numbers to indicate per-layer pruning rate.
+    """
+    # sanity check of provided prune_ratio
+    assert isinstance(prune_ratio, (float, list))
+    n_conv = len([m for m in model.backbone if isinstance(m, nn.Conv2d)])
+    # note that for the ratios, it affects the previous conv output and next
+    # conv input, i.e., conv0 - ratio0 - conv1 - ratio1-...
+    if isinstance(prune_ratio, list):
+        assert len(prune_ratio) == n_conv - 1
+    else:  # convert float to list
+        prune_ratio = [prune_ratio] * (n_conv - 1)
+
+    # we prune the convs in the backbone with a uniform ratio
+    model = copy.deepcopy(model)  # prevent overwrite
+    # we only apply pruning to the backbone features
+    all_convs = [m for m in model.backbone if isinstance(m, nn.Conv2d)]
+    all_bns = [m for m in model.backbone if isinstance(m, nn.BatchNorm2d)]
+    # apply pruning. we naively keep the first k channels
+    assert len(all_convs) == len(all_bns)
+    for i_ratio, p_ratio in enumerate(prune_ratio):
+        prev_conv = all_convs[i_ratio]
+        prev_bn = all_bns[i_ratio]
+        next_conv = all_convs[i_ratio + 1]
+        original_channels = prev_conv.out_channels  # same as next_conv.in_channels
+        n_keep = get_num_channels_to_keep(original_channels, p_ratio)
+
+        # prune the output of the previous conv and bn
+        prev_conv.weight.set_(prev_conv.weight.detach()[:n_keep])
+        prev_bn.weight.set_(prev_bn.weight.detach()[:n_keep])
+        prev_bn.bias.set_(prev_bn.bias.detach()[:n_keep])
+        prev_bn.running_mean.set_(prev_bn.running_mean.detach()[:n_keep])
+        prev_bn.running_var.set_(prev_bn.running_var.detach()[:n_keep])
+
+        # prune the input of the next conv (hint: just one line of code)
+        ##################### YOUR CODE STARTS HERE #####################
+        next_conv.weight.set_(next_conv.weight.detach()[:, :n_keep])
+        ##################### YOUR CODE ENDS HERE #####################
+
+    return model
+
 @torch.no_grad()
 def sensitivity_scan(model, dataloader, scan_step=0.1, scan_start=0.4, scan_end=1.0, verbose=True):
     sparsities = np.arange(start=scan_start, stop=scan_end, step=scan_step)
@@ -465,3 +519,18 @@ if __name__ == "__main__":
     print(f"Sparse model final size={final_sparse_model_size / MiB:.2f} MiB = {final_sparse_model_size / dense_model_size * 100:.2f}% of dense model size")
     final_sparse_model_accuracy = evaluate(model, dataloader['test'])
     print(f"Sparse model accuracy after fine-tuning: {final_sparse_model_accuracy:.2f}%")
+
+    # Question 6
+    print("\n--- Question 6 ---")
+    recover_model()
+    dense_model_accuracy = evaluate(model, dataloader['test'])
+    print(f"dense model has accuracy={dense_model_accuracy:.2f}%")
+
+    dummy_input = torch.randn(1, 3, 32, 32).cuda()
+    pruned_model = channel_prune(model, prune_ratio=0.3)
+    pruned_macs = get_model_macs(pruned_model, dummy_input)
+    assert pruned_macs == 305388064
+    print('* Check passed. Right MACs for the pruned model.')
+
+    pruned_model_accuracy = evaluate(pruned_model, dataloader['test'])
+    print(f"pruned model has accuracy={pruned_model_accuracy:.2f}%")
